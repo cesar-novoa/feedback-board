ac4adef56b2609fface7cb2b2ccf6e4f5d89dbb1
César Nóvoa <themotokar@gmail.com>
2025-09-28 18:46:59 +0100

    Add form components: Form, Input, Label, Textarea, and their context providers

---
+++ src/components/ui/form/index.ts
@@ -0,0 +1,27 @@
+import {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./Form";
+import { Input } from "./Input";
+import { Textarea } from "./Textarea";
+import { Label } from "./Label";
+
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+  Input,
+  Textarea,
+  Label,
+};
