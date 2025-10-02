9e0339c530faee399e32a416274715c19103f796
César Nóvoa <themotokar@gmail.com>
2025-10-02 18:38:52 +0100

    styles changes, add componets common classes on a constant and move cn util to a different folder

---
+++ src/constants/components/form/inputs.ts
@@ -0,0 +1,7 @@
+const INPUT_CLASSES = {
+  FOCUS:
+    "focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-[3px]",
+  ARIA: "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+};
+
+export { INPUT_CLASSES };
